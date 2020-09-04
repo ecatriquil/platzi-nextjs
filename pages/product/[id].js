f18e7fb0a64dd069674eb07d631e54dc4486665f
Emanuel Catriquil <emacatriquil@gmail.com>
2020-09-04 13:35:40 -0300

    Se crean las rutas basicas

---
+++ pages/product/[id].js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {useRouter} from 'next/router'
+
+const ProductItem = () => {
+    const {query: {id}} = useRouter()
+
+    return(
+        <div>
+            Esta es la pagina del producto: {id}
+        </div>
+    )
+}
+
+export default ProductItem
